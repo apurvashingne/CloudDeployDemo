816b9e4d22660c3522a269c2be03c79bf7d0d65c
apurvashingne <86821847+apurvashingne@users.noreply.github.com>
2022-04-26 10:41:16 +0530

    Update index.php

--- example/priyanka-php-apche/index.php
+++ example/priyanka-php-apche/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1> Hello World From Priyanka!! </h1>";
+echo "<h1> Hello World From Prajaktaa!! </h1>";
 $name = getenv('NAME', true) ?: 'World';
 echo sprintf('Hello %s!', $name);
 ?>
